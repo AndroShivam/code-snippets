3e215256a57fff33c06aebf2e157877cc70030b7
AndroShivam <52043419+AndroShivam@users.noreply.github.com>
2020-11-21 19:11:18 +0530

    Create FirebaseUI RecyclerView Adapter.kt

---
+++ Android/FirebaseUI RecyclerView Adapter.kt
@@ -0,0 +1,62 @@
+package com.shivam.makemyday
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+
+
+class Adapter(
+    val options: FirestoreRecyclerOptions<Message>,
+    val listener: OnItemClickListener
+) :
+    FirestoreRecyclerAdapter<Message, UserViewHolder>(options) {
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UserViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.items_messages, parent, false)
+        return UserViewHolder(view, listener, options)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int, model: Message) {
+        holder.senderUserName.text = model.senderUserName
+        holder.senderMessage.text = model.senderMessage
+    }
+}
+
+class UserViewHolder(
+    itemView: View,
+    val listener: OnItemClickListener,
+    private val options: FirestoreRecyclerOptions<Message>
+) :
+    RecyclerView.ViewHolder(itemView),
+    View.OnClickListener {
+
+    var senderUserName: TextView = itemView.findViewById(R.id.sender_name)
+    var senderMessage: TextView = itemView.findViewById(R.id.sender_message)
+
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        listener.onItemClick(
+            documentSnapshot = options.snapshots.getSnapshot(adapterPosition),
+            position = adapterPosition
+        )
+    }
+}
+
+data class ReceivedMessage(
+    var senderUserName: String = "sender_username",
+    var senderUserID: String = "sender_user_id",
+    var senderMessage: String = "message"
+)
